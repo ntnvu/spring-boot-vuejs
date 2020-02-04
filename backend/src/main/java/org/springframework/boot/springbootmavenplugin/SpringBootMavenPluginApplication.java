3264e476ed940b5cd92fdd72761b4ded097cb01b
ngocvu_arosdorados <ngocvu@arosdorados.vn>
2020-02-04 19:34:41 +0700

    Finish step_1

---
+++ backend/src/main/java/org/springframework/boot/springbootmavenplugin/SpringBootMavenPluginApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.springbootmavenplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenPluginApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/springframework/boot/springbootmavenplugin/SpringBootMavenPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.springbootmavenplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMavenPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
